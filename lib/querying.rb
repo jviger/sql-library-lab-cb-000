3e14f11652c7d2119ac6c6f0459bb7a36d90319c
Jason Viger <jason.viger@gmail.com>
2018-08-16 17:36:38 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "select "
+  "select series_name from (select series_name, count(character_id) from characters a left join series b on a.series_id = b.id where species = 'human' group by b.series_name order by count(character_id) desc) d limit 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
