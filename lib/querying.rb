2d246890815c03e645e26908f0ad048bd199743c
Jason Viger <jason.viger@gmail.com>
2018-08-16 17:31:13 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,18 +1,18 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "select title, year from books where series_id = 1 order by year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "select name, motto from characters order by length(motto) desc limit 1"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "select species, count(id) from characters group by species order by count(id) desc limit 1"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "select name, subgenre from authors a left join sub"
 end
 
 def select_series_title_with_most_human_characters
